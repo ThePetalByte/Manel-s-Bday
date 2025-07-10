b6f1260e0c25eaba9262bd871126232009f37728
Imad  🪶 <3kropixx@gmail.com>
2025-07-10 14:53:45 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -5,8 +5,13 @@ const frame2 = document.getElementById('frame2')
 const cakeImg = document.getElementById('cake-img') 
 const lightItBtn = document.getElementById('light-it-btn')
 const yay = document.getElementById("yay")
-const frame = document.getElementById('frame3')
+const frame3 = document.getElementById('frame3')
 const studioBtn = document.getElementById('studio-btn')
+const frame4 = document.getElementById('frame4')
+const takeBtn = document.getElementById('take-a-pic-btn')
+const frame5 = document.getElementById('frame5')
+
+
 
 
 yayBtn.addEventListener('click', () => {
@@ -60,6 +65,34 @@ lightItBtn.addEventListener('click', () => {
 })
 
 studioBtn.addEventListener('click', () => {
-    
+    frame3.classList.add("fade-out");
+    frame3.addEventListener("animationend", () => {
+        frame3.style.display = "none";
+
+        // Show frame2 but keep it invisible first
+        frame4.style.display = 'block';
+        frame4.style.opacity = 0;
+
+        // Force reflow so browser "sees" the opacity change
+        void frame4.offsetWidth;
+
+        // Now add fade-in class
+        frame4.classList.add("fade-in");})
 })
 
+
+takeBtn.addEventListener('click', () => {
+    frame4.classList.add("fade-out");
+    frame4.addEventListener("animationend", () => {
+        frame4.style.display = "none";
+
+        // Show frame2 but keep it invisible first
+        frame5.style.display = 'block';
+        frame5.style.opacity = 0;
+
+        // Force reflow so browser "sees" the opacity change
+        void frame5.offsetWidth;
+
+        // Now add fade-in class
+        frame5.classList.add("fade-in");})
+})
